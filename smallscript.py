5958c0da0caef52f485d85ecea6fc859089d4e5a
jdebelis <joshua.debelis@gmail.com>
2024-02-13 20:51:45 +0000

    SSH and paramiko

---
+++ smallscript.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""this project prints to screen"""
+
+def main():
+
+    mynote = "print this string"    #defining mynote
+    print(mynote)                   #print mynote
+
+main()
